8e324895c613d42627d3237c3af40dc4e0ecf34d
rajdipsaha-IIT-KGP <rajdipsaha7697@gmail.com>
2025-06-26 09:46:04 +0530

    adding routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Signup from './Pages/Signup';
 import Signin from './Pages/Signin';
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/signin" />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/signin" element={<Signin />} />
     </Routes>
