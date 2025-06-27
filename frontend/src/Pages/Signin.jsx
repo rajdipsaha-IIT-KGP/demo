0144d64ef4fadfa0a5dfee98b27f12c7d49a1930
rajdipsaha-IIT-KGP <rajdipsaha7697@gmail.com>
2025-06-27 20:06:22 +0530

    fixing a bug

--- frontend/src/Pages/Signin.jsx
+++ frontend/src/Pages/Signin.jsx
@@ -44,8 +44,8 @@ function Signin() {
         localStorage.setItem('user',JSON.stringify(response.data.user))
       }
       else{
-         sessionStorage.setItem('token', response.data.token);
-        sessionStorage.setItem('user',JSON.stringify(response.data.user))
+         localStorage.setItem('token', response.data.token);
+        localStorage.setItem('user',JSON.stringify(response.data.user))
       }
         setEmail('');
         setName('');
